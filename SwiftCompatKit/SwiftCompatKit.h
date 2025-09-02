3b25e776217d3a6fc06d60a135a2815a0cf2b718
Joshua Walraven <jwalraven12@gmail.com>
2025-09-02 15:26:09 -0700

    Initial Commit

---
+++ SwiftCompatKit/SwiftCompatKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCompatKit.h
+//  SwiftCompatKit
+//
+//  Created by JWI on 2/09/2025.
+//  Copyright © 2025 JWI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCompatKit.
+FOUNDATION_EXPORT double SwiftCompatKitVersionNumber;
+
+//! Project version string for SwiftCompatKit.
+FOUNDATION_EXPORT const unsigned char SwiftCompatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCompatKit/PublicHeader.h>
+
+
