a9effe9b48045137be49ecded179664c2775d101
J_W_I_ <64947174+JWIMaster@users.noreply.github.com>
2025-09-10 09:56:08 +1000

    Update UIFont.swift

--- SwiftCompatKit/SwiftCompatKit/Swift 3/UIFont.swift
+++ SwiftCompatKit/SwiftCompatKit/Swift 3/UIFont.swift
@@ -7,10 +7,53 @@
 //
 
 import Foundation
+// UIFont+Swift3Backport.swift
+// Swift 3 style API available in Swift 2.2
+
 import UIKit
 
+// MARK: - Swift 3 style UIFont methods
 extension UIFont {
     
-    class func preferredFont(forTextStyle: UIFontTextStyle)
+    // System Fonts
+    class func systemFont(ofSize fontSize: CGFloat) -> UIFont {
+        return UIFont.systemFontOfSize(fontSize)
+    }
+    
+    class func boldSystemFont(ofSize fontSize: CGFloat) -> UIFont {
+        return UIFont.boldSystemFontOfSize(fontSize)
+    }
+    
+    class func italicSystemFont(ofSize fontSize: CGFloat) -> UIFont {
+        return UIFont.italicSystemFontOfSize(fontSize)
+    }
+    
+    class func systemFont(ofSize fontSize: CGFloat, weight: CGFloat) -> UIFont {
+        return UIFont.systemFontOfSize(fontSize, weight: weight)
+    }
+    
+    // Preferred Fonts
+    class func preferredFont(forTextStyle style: String) -> UIFont {
+        return UIFont.preferredFontForTextStyle(style)
+    }
+    
+    // Font Names
+    class func fontNames(forFamilyName familyName: String) -> [String] {
+        return UIFont.fontNamesForFamilyName(familyName)
+    }
+}
+
+// MARK: - Swift 3 style UIFontTextStyle struct
+struct UIFontTextStyle {
+    static let body = UIFontTextStyleBody
+    static let headline = UIFontTextStyleHeadline
+    static let subheadline = UIFontTextStyleSubheadline
+    static let footnote = UIFontTextStyleFootnote
+    static let caption1 = UIFontTextStyleCaption1
+    static let caption2 = UIFontTextStyleCaption2
+    static let callout = UIFontTextStyleCallout
+    static let title1 = UIFontTextStyleTitle1
+    static let title2 = UIFontTextStyleTitle2
+    static let title3 = UIFontTextStyleTitle3
 }
 
