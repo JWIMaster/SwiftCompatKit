a509cc9659638792c1cbdf8c58ba053c64655538
Joshua Walraven <jwalraven12@gmail.com>
2025-09-10 09:55:28 +1000

    Create UIFont

---
+++ SwiftCompatKit/SwiftCompatKit/Swift 3/UIFont.swift
@@ -0,0 +1,16 @@
+//
+//  UIFont.swift
+//  SwiftCompatKit
+//
+//  Created by JWI on 10/09/2025.
+//  Copyright © 2025 JWI. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    
+    class func preferredFont(forTextStyle: UIFontTextStyle)
+}
+
